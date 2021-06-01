6cf3a307b7e0943f58575f2b5caba748fb2eff57
daudi13 <davouma13@gmail.com>
2021-06-01 15:26:37 +0300

    commit first js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const smallglasses = document.querySelectorAll('.cup-small');
+ const smallglasses = document.querySelectorAll('.cup-small');
 const liters = document.getElementById('liters');
 const percentage = document.getElementById('percentage');
 const remained = document.getElementById('remained');
@@ -8,3 +8,15 @@ smallglasses.forEach((glass, idx) => {
     glass.addEventListener('click', () => fillglasses(idx))
 })
 
+function fillglasses(idx) {
+    if (smallglasses[idx].classList.contains('full') && !smallglasses[idx].nextElementSibling.classList.contains('full')) {
+        idx--
+    }
+    smallglasses.forEach((glass, idx2) => {
+        if (idx2 <= idx) {
+            glass.classList.add('full')
+        } else {
+            glass.classList.remove('full')
+        }
+    } )
+}
